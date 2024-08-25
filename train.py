e9e34cb0cd218e73d591171494e588ef011ba0f6
Max Weber <maxweber555@gmail.com>
2024-08-25 18:00:33 -0300

    add pipeline e teste_train

--- train.py
+++ train.py
@@ -23,74 +23,73 @@ def reset_seeds():
     np.random.seed(42)
     random.seed(42)
 
-# %% [markdown]
-# # 2 - Fazendo a leitura do dataset e atribuindo às respectivas variáveis
+
+def read_data():
+    data = pd.read_csv(
+        'https://raw.githubusercontent.com/renansantosmendes/lectures-cdas-2023/master/fetal_health_reduced.csv')
+
+    X = data.drop(["fetal_health"], axis=1)
+    y = data["fetal_health"]
+
+    return X, y
 
 
-# %%
-data = pd.read_csv(
-    'https://raw.githubusercontent.com/renansantosmendes/lectures-cdas-2023/master/fetal_health_reduced.csv')
+def process_data(X, y):
+    columns_names = list(X.columns)
+    scaler = preprocessing.StandardScaler()
+    X_df = scaler.fit_transform(X)
+    X_df = pd.DataFrame(X_df, columns=columns_names)
 
-# %% [markdown]
-# # 3 - Preparando o dado antes de iniciar o treino do modelo
+    X_train, X_test, y_train, y_test = train_test_split(X_df,
+                                                        y,
+                                                        test_size=0.3,
+                                                        random_state=42)
 
-# %%
-X = data.drop(["fetal_health"], axis=1)
-y = data["fetal_health"]
+    y_train = y_train - 1
+    y_test = y_test - 1
 
-columns_names = list(X.columns)
-scaler = preprocessing.StandardScaler()
-X_df = scaler.fit_transform(X)
-X_df = pd.DataFrame(X_df, columns=columns_names)
+    return X_train, X_test, y_train, y_test
 
-X_train, X_test, y_train, y_test = train_test_split(X_df,
-                                                    y,
-                                                    test_size=0.3,
-                                                    random_state=42)
 
-y_train = y_train - 1
-y_test = y_test - 1
+def create_model(X):
+    reset_seeds()
+    model = Sequential()
+    model.add(InputLayer(input_shape=(X.shape[1], )))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    return model
 
-# %% [markdown]
-# # 4 - Criando o modelo e adicionando as camadas
 
-# %%
-reset_seeds()
-model = Sequential()
-model.add(InputLayer(input_shape=(X_train.shape[1], )))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(3, activation='softmax'))
+def config_mlflow():
+    # 'renansantosmendes'
+    os.environ['MLFLOW_TRACKING_USERNAME'] = 'maxweber555'
+    # '6d730ef4a90b1caf28fbb01e5748f0874fda6077'
+    os.environ['MLFLOW_TRACKING_PASSWORD'] = '0304bf1fb7fc02b53812bd5a2c0b4807ef4788ec'
+    mlflow.set_tracking_uri(
+        # 'https://dagshub.com/renansantosmendes/mlops-ead.mlflow')
+        'https://dagshub.com/maxweber555/my-first-repo.mlflow')
 
-# %% [markdown]
-# # 5 - Compilando o modelo
-#
+    mlflow.tensorflow.autolog(log_models=True,
+                              log_input_examples=True,
+                              log_model_signatures=True)
 
-# %%
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
 
-# %%
+def train_model(model,X_train, y_train, is_train=True):
+    with mlflow.start_run(run_name='experiment_mlops_ead') as run:
+        model.fit(X_train,
+                  y_train,
+                  epochs=50,
+                  validation_split=0.2,
+                  verbose=3)
 
-os.environ['MLFLOW_TRACKING_USERNAME'] = 'maxweber555'  # 'renansantosmendes'
-# '6d730ef4a90b1caf28fbb01e5748f0874fda6077'
-os.environ['MLFLOW_TRACKING_PASSWORD'] = '0304bf1fb7fc02b53812bd5a2c0b4807ef4788ec'
-mlflow.set_tracking_uri(
-    # 'https://dagshub.com/renansantosmendes/mlops-ead.mlflow')
-    'https://dagshub.com/maxweber555/my-first-repo.mlflow')
 
-mlflow.tensorflow.autolog(log_models=True,
-                          log_input_examples=True,
-                          log_model_signatures=True)
-
-# %% [markdown]
-# # 6 - Executando o treino do modelo
-
-# %%
-with mlflow.start_run(run_name='experiment_mlops_ead') as run:
-    model.fit(X_train,
-              y_train,
-              epochs=50,
-              validation_split=0.2,
-              verbose=3)
+if __name__ == "__main__":
+    X, y = read_data()
+    X_train, X_test, y_train, y_test = process_data(X, y)
+    model = create_model(X)
+    config_mlflow()
+    train_model(model, X_train, y_train)
